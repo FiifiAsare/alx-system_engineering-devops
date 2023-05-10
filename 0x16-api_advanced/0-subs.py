020ad8a8a47568c652f89902b6da5541c0477455
Fiifi Asare - Kumi <107552878+FiifiAsare@users.noreply.github.com>
2023-05-10 02:20:48 +0000

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,20 @@
-#!/bin/usr/python3
-"""number of subcribers for a given subreddit """
+#!/usr/bin/python3
+"""
+number of subscribers for a given subreddit
+"""
 
 from requests import get
 
+
 def number_of_subscribers(subreddit):
-  """function that queries the Reddit API and returns the number of subscribers
-    (not active users, total subscribers) for a given subreddit."""
-  
-  if subreddit is None or not isinstance(subreddit, str):
-    return 0
-  
+    """
+    function that queries the Reddit API and returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit.
+    """
+
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+
     user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     response = get(url, headers=user_agent)
@@ -20,4 +25,3 @@ def number_of_subscribers(subreddit):
 
     except Exception:
         return 0
-  
